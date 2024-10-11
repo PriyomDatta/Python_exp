183686fb8e5d06b18a1fc9fe8121d691ee8a6d0b
PriyomDatta <priyomdat@gmail.com>
2024-10-11 09:28:47 +0530

    Create Anim_001.py
    Creating a sin animation need to verify

---
+++ Animation/Anim_001.py
@@ -0,0 +1,33 @@
+#Animating a gif 
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import PillowWriter
+
+fig = plt.figure()
+l, = plt.plot([], [], 'k--')
+
+plt.xlim(-5,5)
+plt.ylim(-5,5)
+
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.title('Sin plot')
+
+def func(x):
+    return 3*np.sin(x)
+    
+metadata = dict(title='Movie',artist='Nobody')
+writer = PillowWriter(fps = 15,metadate=metadata)
+
+xlist = []
+ylist = []
+
+with writer.saving(fig, "sinWave.gif", 100):
+    for xval in np.linspace(-5,5,100):
+        xlist.append(xval)
+        ylist.append(func(xval))
+        
+        l.set_data(xlist,ylist)
+        
+        writer.grab_frame()

d396b7246cc4bc6a8fa4000ebc6c3b6f8f723f5f
PriyomDatta <priyomdat@gmail.com>
2024-10-26 20:19:26 +0530

    Step 6 achieved

--- Data_analysis/Codes/Mycode_002.py
+++ Data_analysis/Codes/Mycode_002.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from numpy import nan
 from future.builtins.misc import isinstance
+from openpyxl import load_workbook
+import os
 
 file_path = '../UT_data/program_Ford_DCME_metric.xlsx'
 new_file_path = '../UT_data/Data_002_temp1.xlsx'
@@ -19,6 +21,71 @@ def add_empty_rows(df):
         new_df = pd.concat([new_df, df.iloc[[i]]], ignore_index=True)
     return new_df
 
+def extract_prefix(s):
+    return s.split('/')[0]
+
+def formatting_and_save(df_fil1: pd.DataFrame,chec_string: str) -> pd.DataFrame:
+    #Adding extra data as per requirement if MCDC change required
+    df_fil1.loc[:,'C0']='xyz'
+    df_fil1.loc[:,'C1']='xyz'
+    df_fil1.loc[:,'CTE']='Not Created'
+    df_fil1.loc[:,'Code Coverage Status']='abc'
+    df_fil1.loc[:,'Report Generated']='Not Done'
+    df_fil1.loc[:,'Backup Saved']='Not Done'
+
+    # Drop duplicates and keep the first occurrence
+    df_fil1['Metric name'] = df_fil1['Metric name'].where(~df_fil1.duplicated(subset=['Metric name']), nan)
+
+    # Apply the function
+    new_df = add_empty_rows(df_fil1)
+
+    #Upshift first column
+    for i in range((len(new_df) - 1)):
+        new_df.iloc[i,0] = new_df.iloc[(i + 1),0]
+
+    #formatting the last [last,0] element
+    el_last = len(new_df) - 1
+    new_df.iloc[el_last, 0] = np.nan
+
+    #Adding the Si. No.
+    for i in range(len(new_df)):
+        if isinstance(new_df.iloc[i,0],str):
+            ind = 1
+        elif i + 1 < len(new_df) and isinstance(new_df.iloc[i + 1, 0], str):
+            ind = 0
+        else:
+            new_df.iloc[i,0] = ind
+            ind +=1
+
+    #new_df.to_excel(new_file_path,sheet_name=chec_string, index=False)
+    # Check if the file exists
+    if os.path.exists(new_file_path):
+        print(chec_string)
+        append_new_sheet_to_excel(new_df,chec_string)
+        print(chec_string)
+
+    else:
+        new_df.to_excel(new_file_path, sheet_name=chec_string, index=False)
+        print(type(chec_string))
+        
+
+def append_new_sheet_to_excel(df: pd.DataFrame, sheet_name: str,):
+    try:
+        # Open the existing workbook with openpyxl
+        book = load_workbook(new_file_path)
+        
+        # Use pandas ExcelWriter with openpyxl engine, and don't overwrite the existing file
+        with pd.ExcelWriter(new_file_path, mode='a', engine='openpyxl', if_sheet_exists='new') as writer:
+            writer._book = book  # Use the internal book assignment method
+            df.to_excel(writer, sheet_name=sheet_name, index=False)
+        
+        print(f"Successfully added a new sheet '{sheet_name}' to {file_path}.")
+    
+    except Exception as e:
+        print(f"Error while adding new sheet: {e}")
+
+
+
 #............................. Program Starts .............................
 
 df = pd.read_excel(file_path, sheet_name=sheet_name)
@@ -32,37 +99,15 @@ df_fil1 = df_fil1.iloc[:, :2]
 #Change abc with module name, one at a time
 #df_fil1 = df_fil1[df_fil1['Metric name'].str.startswith('abc')]
 
-#Adding extra data as per requirement if MCDC change required
-df_fil1['C0']='xyz'
-df_fil1['C1']='xyz'
-df_fil1['CTE']='Not Created'
-df_fil1['Code Coverage Status']='abc'
-df_fil1['Report Generated']='Not Done'
-df_fil1['Backup Saved']='Not Done'
+prefixes = df_fil1['Metric name'].apply(extract_prefix)
+prefixes = prefixes.drop_duplicates()
 
-# Drop duplicates and keep the first occurrence
-df_fil1['Metric name'] = df_fil1['Metric name'].where(~df_fil1.duplicated(subset=['Metric name']), nan)
+for i in range(len(prefixes)):
+    chec_string = prefixes.iloc[i]  # Access the ith element of prefixes
+    grouped_data = df_fil1[df_fil1['Metric name'].str.startswith(chec_string)]
+    formatting_and_save(grouped_data,chec_string)
 
-# Apply the function
-new_df = add_empty_rows(df_fil1)
+#print(grouped_data)
+#print(prefixes)
 
-#Upshift first column
-for i in range((len(new_df) - 1)):
-    new_df.iloc[i,0] = new_df.iloc[(i + 1),0]
-
-#formatting the last [last,0] element
-el_last = len(new_df) - 1
-new_df.iloc[el_last, 0] = np.nan
-
-#Adding the Si. No.
-for i in range(len(new_df)):
-    if isinstance(new_df.iloc[i,0],str):
-        ind = 1
-    elif i + 1 < len(new_df) and isinstance(new_df.iloc[i + 1, 0], str):
-        ind = 0
-    else:
-        new_df.iloc[i,0] = ind
-        ind +=1
-
-new_df.to_excel(new_file_path, index=False)
 print("script run completed")

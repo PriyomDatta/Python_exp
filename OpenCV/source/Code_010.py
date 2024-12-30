286707e6cf870f31431c881420f1efef63caf0d3
PriyomDatta <priyomdat@gmail.com>
2024-12-31 04:28:54 +0530

    Adding comment

--- OpenCV/source/Code_010.py
+++ OpenCV/source/Code_010.py
@@ -5,10 +5,10 @@ import cv2
 img = cv2.imread('../Images/sudoku.png',0)
 cv2.imshow("Original",img)
 
-ret, thresh_basic = cv2.threshold(img,70,255,cv2.THRESH_BINARY)
+ret, thresh_basic = cv2.threshold(img,70,255,cv2.THRESH_BINARY)     #A simple threshold implementation
 cv2.imshow("Basic Binary",thresh_basic)
 
-thres_adapt = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+thres_adapt = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)    #Adapted threshold implementation
 cv2.imshow("Adaptive Threshold",thres_adapt)
 
 cv2.waitKey(0)

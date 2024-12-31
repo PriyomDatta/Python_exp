778712df8256ef58536bc471c36f992d905fa974
PriyomDatta <priyomdat@gmail.com>
2024-12-31 07:10:34 +0530

    area parameter and centroid of contour

--- OpenCV/source/Code_012.py
+++ OpenCV/source/Code_012.py
@@ -1,4 +1,4 @@
-# Contour
+# Contour   their area, parameter and centroid
 import numpy as np
 import cv2
 
@@ -17,6 +17,24 @@ color = (255, 0, 255)                                   #pimk line
 
 cv2.drawContours(img2, contours, index, color, thickness)   #draw the contours
 cv2.imshow("Contours",img2)
+
+objects = np.zeros([img.shape[0], img.shape[1], 3], 'uint8')        #width x height x chanels  blank canvus
+
+for c in contours:
+	cv2.drawContours(objects, [c], -1, color, -1)
+
+	area = cv2.contourArea(c)                                   # area is pixel^2
+	perimeter = cv2.arcLength(c, True)                          #True means closed 
+
+	M = cv2.moments(c)                                          #finds moment
+	cx = int(M['m10'] / M['m00'])                               #centroid along x
+	cy = int(M['m01'] / M['m00'])                               #centroid along y
+
+	cv2.circle(objects, (cx, cy), 4, (0, 0, 255), -1)           #drawing the centroid
+
+	print(f"Area: {area}, perimeter: {perimeter}")              #printing
+
+cv2.imshow("Contours_centroid", objects)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
